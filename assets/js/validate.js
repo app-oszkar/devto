feec6002e5f8af36b9be0d3850e81ea0d8460f84
KiwiChat <52504536+KiwiChat@users.noreply.github.com>
2022-02-22 15:09:41 +0100

    Update validate.js

--- assets/js/validate.js
+++ assets/js/validate.js
@@ -1,7 +1,22 @@
 function validate()
 {
 var inputVal = document.getElementById("subdomain").value;
-invalidList=["admin","administrator","root","email","webmail","cpanel","irc","chat","webchat","cname","ns","xxx","xx","x","ns"];
+invalidList=[
+"admin",
+"administrator",
+"root","email",
+"webmail",
+"cpanel",
+"irc",
+"chat",
+"webchat",
+"cname",
+"ns",
+"xxx",
+"xx",
+"x",
+"api"
+];
 if(invalidList.indexOf(inputVal) !== -1){
         document.getElementById("submitSubDomain").innerHTML="<p style='color:red'> Invalid SubDomain</p>";
     } else{
@@ -11,7 +26,7 @@ if(invalidList.indexOf(inputVal) !== -1){
 function validateSubmit()
 {
 var inputVal = document.getElementById("subdomain").value;
-invalidList=["admin","administrator","kiwichat"];
+invalidList=["kiwichat"];
 if(invalidList.indexOf(inputVal) !== -1){
         alert("Invalid");
     } else{
