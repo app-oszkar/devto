359ec85db092a103cf3ad177b464a188958d4385
KiwiChat <52504536+KiwiChat@users.noreply.github.com>
2022-02-23 22:26:07 +0100

    Update scripts.js

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,7 +1,14 @@
-/*!
-* Start Bootstrap - Modern Business v5.0.5 (https://startbootstrap.com/template-overviews/modern-business)
-* Copyright 2013-2021 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-modern-business/blob/master/LICENSE)
-*/
-// This file is intentionally blank
-// Use this file to add JavaScript to your project
+        <script type="text/javascript" src="https://devto.ml/assets/js/restricted.js"></script>
+        <script type="text/javascript" >
+            function load() {
+                var mydata = JS.parse(restricted);
+                alert(mydata.length);
+
+                var div = document.getElementById('restricted');
+
+                for(var i = 0;i < mydata.length; i++)
+                {
+                    div.innerHTML = div.innerHTML + "<p class='inner' id="+i+">"+ mydata[i].name +"</p>" + "<br>";
+                }
+            }
+        </script>
